adb3cefb1b416b469acf7e2ba7e682ffec9238d8
Nazrath <nazrath.nawaz@yahoo.de>
2025-04-03 16:53:12 +0100

    extract basic information from raw file

---
+++ ExampleProgram/Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RawFileReaderExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RawFileReaderExample")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e3a1e2ae-954e-4a9f-9be5-983d89b6aeb5")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RawFileReaderQQQTest/RawFileReaderQQQTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using ThermoFisher.CommonCore.Data;
+using ThermoFisher.CommonCore.Data.Business;
+using ThermoFisher.CommonCore.Data.FilterEnums;
+using ThermoFisher.CommonCore.Data.Interfaces;
+using ThermoFisher.CommonCore.RawFileReader;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string rawFilePath = @"C:\\Users\\naz\\Documents\\GitHub\\ThermoRawFileReader\\data\\test.raw";
+        Console.WriteLine("Running from: " + Environment.CurrentDirectory);
+
+        using (IRawDataPlus rawFile = RawFileReaderAdapter.FileFactory(rawFilePath))
+        {
+            if (!rawFile.IsOpen)
+            {
+                Console.WriteLine("❌ Could not open RAW file.");
+                return;
+            }
+
+            rawFile.SelectInstrument(Device.MS, 1);
+
+            Console.WriteLine("RAW file opened successfully!");
+            Console.WriteLine($"File Name: {rawFile.FileName}");
+
+            var instrumentModel = rawFile.GetInstrumentData().Model;
+            Console.WriteLine($"Instrument Model: {instrumentModel}");
+
+            var runHeader = rawFile.RunHeaderEx;
+            Console.WriteLine($"Start Time: {runHeader.StartTime}");
+            Console.WriteLine($"End Time: {runHeader.EndTime}");
+            Console.WriteLine($"Spectra Count: {runHeader.SpectraCount}");
+        }
+    }
+}
